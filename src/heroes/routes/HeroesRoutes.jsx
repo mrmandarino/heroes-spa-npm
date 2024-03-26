4aeadc40e764a03e6c800b7b54eab23fdae7604d
René Avendaño <samuel.benicio.delmar@gmail.com>
2024-03-26 20:34:32 -0300

    proyecto avanzado hasta leccion 195

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { Navbar } from "../../ui/components/Navbar"
+import { MarvelPage } from "../pages/MarvelPage"
+import { DcPage } from "../pages/DcPage"
+import { SearchPage } from "../pages/SearchPage"
+import { HeroPage } from "../pages/HeroPage"
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar/>
+        <div className="container" >
+            <Routes>
+                <Route path="marvel" element={<MarvelPage/>}/>
+                <Route path="dc" element={<DcPage/>}/>
+                
+                <Route path="search" element={<SearchPage/>}/>
+                <Route path="hero" element={<HeroPage/>}/>
+
+                <Route path="/" element={<Navigate to="/marvel"/>}/>
+            </Routes>
+        </div>
+    </>
+  )
+}
